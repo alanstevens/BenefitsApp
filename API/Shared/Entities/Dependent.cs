7fdbd22a99551c55ffdc44dbe35c265172b5a88f
H. Alan Stevens <alanstevens@gmail.com>
2018-06-27 10:57:09 -0400

    remove folder renamin detritus.

--- API/Shared/Entities/Dependent.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Paylocity.API.Shared.Entities
-{
-    public class Dependent : Person
-    {
-        public int EmployeeId { get; set; }
-        public Employee Employee { get; set; }
-    }
-}
